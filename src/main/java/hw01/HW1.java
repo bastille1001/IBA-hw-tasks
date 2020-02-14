7dc38e4ab2d6864428616f28c5971f7e2731b953
Timur Huseynov <timur.physics@mail.ru>
2020-02-15 01:23:12 +0400

    #updated

--- src/main/java/hw01/HW1.java
+++ src/main/java/hw01/HW1.java
@@ -3,17 +3,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW1 {
+    public static int[] add(int[] array, int element) {
+        int[] tmp = array.clone();
+        array = new int[tmp.length+1];
+        for (int i = 0; i < tmp.length; i++) {
+            array[i] = tmp[i];
+        }
+        array[tmp.length] = element;
+        return array;
+    }
     public static void main(String[] args) {
         //генерация случайных чисел [0-100]
         int randomNum = (int) (Math.random() * (100 + 1));
         Scanner in = new Scanner(System.in);
+
         //массив чисел, который буду заполнять
         int[] array = new int[100];
+
         //использовал просто для проверки кода
-        System.out.println(randomNum);
+        //System.out.println(randomNum);
+
         System.out.print("Enter your name: ");
         String name = in.nextLine();
         System.out.println("Let the game begin!");
+
+
         for (int i = 0; i < array.length; i++) {
             int guessNum = in.nextInt();
             array[i] = guessNum;
@@ -22,23 +36,22 @@ public class HW1 {
             } else if (randomNum > guessNum) {
                 System.out.println("Your number is too small. Please, try again.");
             } else {
-                System.out.println("Congratulations, " + name + "!");
+                System.out.printf("Congratulations, %s! \n",name);
+                //пузырьковый метод сортировки
+                for (int k = 0; k < array.length - 1; k++) {
+                    for (int j = k + 1; j < array.length; j++) {
+                        if (array[k] < array[j]) {
+                            int m = array[j];
+                            array[j] = array[k];
+                            array[k] = m;
+                        }
+                    }
+                }
+                System.out.println("Your number is: ");
+                System.out.println(Arrays.toString(add(array,guessNum)));
                 break;
             }
         }
-        //пузырьковый метод сортировки
-        for (int k = 0; k < array.length - 1; k++) {
-            for (int j = k + 1; j < array.length; j++) {
-                if (array[k] < array[j]) {
-                    int m = array[j];
-                    array[j] = array[k];
-                    array[k] = m;
-                }
-            }
-        }
-        String aString = Arrays.toString(array);
-        // вывод на экран введенных чисел
-        System.out.println("Your numbers is: " + aString);
     }
 }
 
